83855037be547a970778ddbd305d6aa54afd0502
Samuel Gomez <samuel.gomez@kaleidos.net>
2020-07-17 12:45:18 +0200

    mustachio

---
+++ precache-manifest.11a1b575bbd1eadc2eda24ea8ae6c6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edabaa54d1fa328c8be54ce9dd1e27f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67e439a993025d518615",
+    "url": "/static/css/main.6c57158c.chunk.css"
+  },
+  {
+    "revision": "d515aee5a2b98af033eb",
+    "url": "/static/js/2.4608cb33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4608cb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e439a993025d518615",
+    "url": "/static/js/main.55401b9b.chunk.js"
+  },
+  {
+    "revision": "623dce94a985447e6023",
+    "url": "/static/js/runtime-main.3370ec22.js"
+  }
+]);
